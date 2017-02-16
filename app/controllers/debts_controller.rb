22b743116a1480198b799b866e785708e106816e
Piotr Wasiak <chooosen@gmail.com>
2017-02-16 11:36:08 +0100

    Readme.md and database.yml.sample

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -1,16 +1,16 @@
 class DebtsController < ApplicationController
   # POST /debts.json
   def create
-    @debt = Debt.new(debt_params)
-
-    if @debt.save
-      render :show, status: :created, location: @debt
-    else
-      render json: @debt.errors, status: :unprocessable_entity
-    end
+    return render json: {}, status: 403 unless valid_slack_token?
+    # TODO: worker.perform_async(command_params.to_h)
+    render json: { responce_type: 'in channel' }, status: :created
   end
 
   private
+
+  def valid_slack_token?
+    params[:token] == ENV['SLACK_SLASH_COMMAND_TOKEN']
+  end
 
   def debt_params
     params.require(:debt).permit(:amount, :amount, :debtor, :creditor)
